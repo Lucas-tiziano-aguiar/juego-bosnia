2ea6c2789a273f1263c0e0072ce81b45adce40c0
Lucas Aguiar <lucasaguiarlafe@gmail.com>
2023-09-01 00:19:17 -0300

    mas

--- play/player.gd
+++ play/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var anim := $AnimatedSprite2D
 var speed := 120;
 var direccion := 0.0;
 const gravity = 9
@@ -9,6 +10,14 @@ func _physics_process(delta):
 	direccion = Input.get_axis("left","right")
 	velocity.x = direccion * speed
 	velocity.y = velocity.y + gravity
+	if direccion != 0:
+		anim.play("run")
+	else:
+		anim.play("iddle")
+	if direccion > 0:
+		anim.flip_h= false
+	else:
+		anim.flip_h = true
 	
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y -= jump
