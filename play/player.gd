27dff4d49c3250283eb828deea77ac2ef0e2b739
VinlandSagq <arzamendianazarenoj2004@gmail.com>
2023-09-04 19:52:01 -0300

    caminar

--- play/player.gd
+++ play/player.gd
@@ -11,19 +11,20 @@ func _physics_process(delta):
 	if Input.is_action_pressed("sprint"):
 		speed = 200
 	else:
+	
 		speed = 120
 	
 	velocity.x = direccion * speed
 	velocity.y = velocity.y + gravity
 	if direccion != 0:
-		anim.play("run")
+		anim.play("caminar")
+	
 	else:
 		anim.play("iddle")
 	if direccion > 0:
 		anim.flip_h= false
 	else:
 		anim.flip_h = true
-	
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		velocity.y -= jump
 	move_and_slide()
