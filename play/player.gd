c6b3c3b9f7ffa883a3b990bd1bec37f55dde0aaa
VinlandSagq <arzamendianazarenoj2004@gmail.com>
2023-10-05 19:19:21 -0300

    godotV4

---
+++ play/player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+@onready var anim := $AnimatedSprite2D
+var speed := 120;
+var direccion := 0.0;
+const gravity = 9
+const jump = 250
+
+func _physics_process(delta):
+	direccion = Input.get_axis("left","right")
+	velocity.x = direccion * speed
+	velocity.y = velocity.y + gravity
+	if direccion != 0:
+		if Input.is_action_pressed("sprint"):
+			anim.play("run")
+			speed = 200
+		else:
+			anim.play("caminar");
+			speed = 120
+	else:
+		anim.play("iddle")
+
+	if direccion > 0:
+		anim.flip_h= false
+	else:
+		anim.flip_h = true
+		
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y -= jump
+	move_and_slide()
