2f5f00432b915ddfea7ecfceb104571e15acb6e8
Lucas Aguiar <lucasaguiarlafe@gmail.com>
2023-10-17 22:15:32 -0300

    funcionAgachar

--- play/player.gd
+++ play/player.gd
@@ -1,22 +1,60 @@
 extends Humano
 
+@onready var parado = $Parado;
+@onready var arrastrado = $Arrastrado
+@onready var anim := $Animacion
+
+func _ready():
+	parado.disabled = false
+	arrastrado.disabled = true
+
 func _physics_process(delta):
-	direccion = Humano.inputTeclasLeftRight()
+	if is_on_floor():
+		direccion = Humano.inputTeclasLeftRight()
 	velocity.x = direccion * speed
 	velocity.y = velocity.y + gravity
+	salto()
 	if direccion != 0 and is_on_floor():
-		if Input.is_action_pressed("sprint"):
-			anim.play("run")
-			speed = 140
-		else:
-			anim.play("caminar");
-			speed = 120
+		direccionSprite()
+		agacharse()
+		sprint()
 	else:
-		anim.play("iddle")
+		iddle()
+	move_and_slide()
+
+func sprint():
+	if Input.is_action_pressed("sprint"):
+		anim.play("correr")
+		speed = 140
+	else:
+		caminar()
+	
+
+func caminar():
+	anim.play("caminar")
+	speed = 120
+
+func iddle():
+	anim.play("iddle")
+
+func direccionSprite():
 	if direccion > 0:
 		anim.flip_h= false
 	else:
 		anim.flip_h = true
+
+func agacharse():
+	if Input.is_action_pressed("crouch"):
+		anim.play("agacharse")
+		anim.play("agachado")
+		parado.disabled = true
+		arrastrado.disabled = false
+	elif Input.is_action_just_released("crouch"):
+		anim.play("levantarse")
+		parado.disabled = false
+		arrastrado.disabled = true
+
+func salto():
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		anim.play("saltar")
 		velocity.y -= jump
-	move_and_slide()
