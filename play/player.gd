7c9c948e87bc7b6a7d7bb4a462e53ff04a0f0f41
VinlandSagq <arzamendianazarenoj2004@gmail.com>
2023-11-14 20:24:30 -0300

    Revert "Merge branch 'lucas'"
    This reverts commit 24ba4fc8b162649fb525f81b84fbe0dce8db7eec, reversing
    changes made to ec21b68b643ad3fbf6a60db47983c3ecaf651c6a.

--- play/player.gd
+++ play/player.gd
@@ -1,48 +1,36 @@
 extends Humano
-@export var climbing = false;
 
 @onready var parado = $Parado;
 @onready var arrastrado = $Arrastrado
 @onready var anim := $Animacion
+var  raycastLong = 11.5
 
-var trepado = false;
 var blend = 0.5
-<<<<<<< Updated upstream
-=======
 var agarrado = false
-
->>>>>>> Stashed changes
 func _ready():
 	parado.disabled = false
 	arrastrado.disabled = true
+	$raycastParkour.target_position.y = raycastLong
 
 func _physics_process(delta):
-	if is_on_floor():
-		direccion = Humano.inputTeclasLeftRight()
-<<<<<<< Updated upstream
-		trepado = false;
-	velocity.x = direccion * speed
-	velocity.y = velocity.y + gravity
-<<<<<<< Updated upstream
-	salto()
-	if direccion != 0 and is_on_floor() and trepado = false:
-=======
-	if direccion != 0 and is_on_floor():
->>>>>>> Stashed changes
-		direccionSprite()
-		agacharse()
-		sprint()
-		salto()
-	else:
-		if is_on_floor_only():
+	agarrarse()
+	if agarrado == false and arrastrado.disabled == true:
+		if is_on_floor():
+			direccion = Humano.inputTeclasLeftRight()
+		velocity.x = direccion * speed
+		velocity.y += gravity
+		if direccion != 0 and is_on_floor():
+			direccionSprite()
+			sprint()
+			salto()
+		elif is_on_floor():
 			iddle()
-		else:
-			pass
-=======
+	if agarrado == true and arrastrado.disabled == true:
+		direccion = Humano.inputTeclasLeftRight()
 		velocity.x = 0
 		velocity.y = 0
+		direccionSprite()
 		saltoAgarrado()
->>>>>>> Stashed changes
 	move_and_slide()
 
 func sprint():
@@ -62,19 +50,16 @@ func iddle():
 func direccionSprite():
 	if direccion > 0:
 		anim.flip_h= false
-	else:
+		$raycastParkour.target_position.y = raycastLong
+	elif direccion < 0:
 		anim.flip_h = true
-<<<<<<< Updated upstream
-=======
 		$raycastParkour.target_position.y = -raycastLong
-
 
 func estado():
 	if speed > 0:
 		sprint()
 	elif speed == 0:
 		iddle()
->>>>>>> Stashed changes
 
 func agacharse():
 	if Input.is_action_pressed("crouch"):
@@ -87,9 +72,15 @@ func agacharse():
 		parado.disabled = false
 		arrastrado.disabled = true
 
-<<<<<<< Updated upstream
-=======
 func saltoAgarrado():
+	if Input.is_action_just_pressed("jump"):
+		anim.play("saltar")
+		velocity.y -= jump
+		agarrado = false
+		arrastrado.disabled = true
+
+
+func salto():
 	if Input.is_action_just_pressed("jump"):
 		anim.play("saltar")
 		direccionSprite()
@@ -97,30 +88,15 @@ func saltoAgarrado():
 		agarrado = false
 		arrastrado.disabled = true
 
-func subirEscalera(): 
-	if Input.is_action_pressed("up"):
-		velocity.y += speed * 5
-	elif Input.is_action_pressed("down"):
-		velocity.y = speed
-
->>>>>>> Stashed changes
-func salto():
-	if Input.is_action_just_pressed("jump"):
-		anim.play("saltar")
-		velocity.y -= jump
-
-<<<<<<< Updated upstream
-
-func _on_area_2d_body_entered(body):
-	$Area2D/delante
-=======
 func agarrarse():
 	if ($raycastParkour.get_collider()):
 		print($raycastParkour.get_collider().is_in_group("tilesAgarrables"))
-		if $raycastParkour.get_collider().is_in_group("tilesAgarrables") and Input.is_action_pressed("agarrarse"):
-				anim.play("agarrado")
-				agarrado = true
-		elif Input.is_action_just_released("agarrarse"):
+		if $raycastParkour.get_collider().is_in_group("tilesAgarrables"):
+				if Input.is_action_pressed("agarrarse"):
+					anim.play("agarrado")
+					agarrado = true
+				elif Input.is_action_just_released("agarrarse"):
+					agarrado = false
+		else:
 			anim.play("saltar")
 			agarrado = false
->>>>>>> Stashed changes
